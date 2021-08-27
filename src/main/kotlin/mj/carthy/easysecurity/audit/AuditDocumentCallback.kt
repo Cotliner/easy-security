21f292959c1752f198dee52199149e8032a24a1e
Carthy.Marie-Joseph <carthymariejoseph@gmail.com>
2021-08-27 12:59:53 +0200

    code: Improuvement

--- src/main/kotlin/mj/carthy/easysecurity/audit/AuditDocumentCallback.kt
+++ src/main/kotlin/mj/carthy/easysecurity/audit/AuditDocumentCallback.kt
@@ -8,6 +8,7 @@ import mj.carthy.easyutils.document.BaseDocument
 import org.reactivestreams.Publisher
 import org.springframework.data.mongodb.core.mapping.event.ReactiveBeforeConvertCallback
 import org.springframework.security.core.context.ReactiveSecurityContextHolder
+import org.springframework.security.core.context.ReactiveSecurityContextHolder.getContext
 import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.core.userdetails.UserDetails
 import reactor.core.publisher.Mono
@@ -36,29 +37,27 @@ class AuditDocumentCallback : ReactiveBeforeConvertCallback<BaseDocument<UUID?>>
     documentToAudit: T,
     username: String,
     date: Instant
-  ) {
-    documentToAudit.id = UUID.randomUUID()
-    documentToAudit.createdBy = username
-    documentToAudit.createdDate = date
+  ): Unit = with(documentToAudit) {
+    this.id = UUID.randomUUID()
+    this.createdBy = username
+    this.createdDate = date
   }
 
   @VisibleForTesting fun <T : BaseDocument<UUID?>> setLastModifiedDate(
     documentToAudit: T,
     username: String,
     date: Instant
-  ) {
-    documentToAudit.lastModifiedBy = username
-    documentToAudit.lastModifiedDate = date
+  ): Unit = with(documentToAudit) {
+    this.lastModifiedBy = username
+    this.lastModifiedDate = date
   }
 
-  @VisibleForTesting suspend fun username(): String = ReactiveSecurityContextHolder.getContext().map {
-    getCurrentAuditor(it)
-  }.switchIfEmpty(Mono.just(SYSTEM)).awaitSingle()
+  @VisibleForTesting suspend fun username(): String = getContext().map { it.getCurrentAuditor() }.awaitSingle() ?: SYSTEM
 
-  @VisibleForTesting fun getCurrentAuditor(auth: SecurityContext): String = when (auth.authentication) {
-    is UserTokenAuth -> when (auth.authentication.principal) {
-      is UserDetails -> (auth.authentication.principal as UserDetails).username
-      is String -> auth.authentication.principal as String
+  @VisibleForTesting fun SecurityContext.getCurrentAuditor(): String = when (this.authentication) {
+    is UserTokenAuth -> when (this.authentication.principal) {
+      is UserDetails -> (this.authentication.principal as UserDetails).username
+      is String -> this.authentication.principal as String
       else -> SYSTEM
     }
     else -> SYSTEM
